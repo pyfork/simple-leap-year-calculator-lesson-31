66ebf290ab70e3ad1bf1f56ce63bf3cda9d62be4
Ahmad <118096225+pyfork@users.noreply.github.com>
2022-11-23 13:57:51 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Simple Leap Year Calculator (Python)
+
+Checks if a given year is a leap year.
+
+
+#### Side note
+
+- Lesson 31
+- Use draw.io to visualize the logic. Draw first. Read logic. Then write code.
+- Nested if statements in summary:
+  - inner if statement will be checked if outer if statement is deemed true
+- Elif statements in summary:
+  - next elif will be checked if the prior if statement is deemed not true
+- New to Python and not so sharp with logic when first attempted this
+- Took me 2 days to bend my mind
+- Formula [Mathisfun's definition of leap year](https://www.mathsisfun.com/leap-years.html)
+- Words:
+  - Don't give up on yourself
+  - Keep going
---
+++ main.py
@@ -0,0 +1,21 @@
+# Get year from user
+y = int(input("Which year do you want to check?"))
+
+print(y)
+
+l = "Leap year"
+n = "Normal year"
+
+# Can the year be divided by 4 with no remainder? Yes-next if; No-normal
+# Can the year be divided by 100 with no remainder? Yes-next if; No-leap
+# Can the year be divided by 400 with no remainder? Yes-leap; No-normal
+if (y % 4 == 0):
+  if (y % 100 == 0):
+    if (y % 400 == 0):
+      print(f"Divisible by 4, by 100 and by 400. Hooray! {y} is a {l}")
+    else:
+      print(f"Divisible by 4 and by 100 but not by 400 so this is a {n}")
+  else:
+    print(f"Divisible by 4 but not by 100 so this {y} is a {l} ")
+else:
+  print(f"Not divisible by 4 so it's a {n}")
